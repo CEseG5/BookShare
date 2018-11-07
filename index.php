3081cfdc38203f7fccd8517b726d628ceec66492
ukaltrine <ukaltrine@users.noreply.github.com>
2018-11-07 15:12:05 +0100

    added include statements, added an area for the latest books

--- index.php
+++ index.php
@@ -1,239 +1,81 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>uiCookies:Inspire &mdash; BookShare</title>
-    <meta name="description" content="Free Bootstrap Theme by uicookies.com">
-    <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
-    
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,500,700">
-    <link rel="stylesheet" href="css/styles-merged.css">
-    <link rel="stylesheet" href="css/style.min.css">
+<?php include "includes/header.php" ?>
 
-  </head>
-  <body>
-    
-    <!-- Fixed navbar -->
-    <!-- navbar-fixed-top  -->
-        
-    <nav class="navbar probootstrap-megamenu navbar-default probootstrap-navbar">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="index.php" title="uiCookies:Inspire">Inspire</a>
-        </div>
+<section class="probootstrap-hero">
+  <div class="container">
 
-        <div id="navbar-collapse" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="index.php">Home</a></li>
-			<li><a href="faq.php">FAQ</a></li>
-            <li><a href="contact.php">Contact</a></li>
-             <li class="probootstra-cta-button"><a href="#" class="btn" data-toggle="modal" data-target="#loginModal">Log in</a></li>
-            <li class="probootstra-cta-button last"><a href="#" class="btn btn-ghost" data-toggle="modal" data-target="#signupModal">Sign up</a></li>
-			
-          </ul>
-        </div>
-      </div>
-    </nav>
-	   
-
-    <section class="probootstrap-hero">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 text-center probootstrap-hero-text pb0 probootstrap-animate" data-animate-effect="fadeIn">
-            <h1>Share your books!</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
-            <p>
-              <a href="#" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#loginModal">Log In</a>
-              <a href="#" class="btn btn-primary btn-ghost btn-lg" data-toggle="modal"  data-target="#signupModal">Sign Up</a>
-            </p>
-            <!-- <p><a href="#"><i class="icon-play2"></i> Watch the video</a></p> -->
-          </div>
-        </div>
-
-        <div class="row probootstrap-feature-showcase">
-          <div class="col-md-4 col-md-push-8 probootstrap-showcase-nav probootstrap-animate">
-            <ul>
-              <li class="active">
-                <a href="#">Responsive Design</a>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
-              </li>
-              <li>
-                <a href="#">Business Solution</a>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
-              </li>
-              <li>
-                <a href="#">Brand Identity</a>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
-              </li>
-              <li>
-                <a href="#">Creative Ideas</a>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
-              </li>
-              <li>
-                <a href="#">Search Engine Friendly</a>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
-              </li>
-              <li>
-                <a href="#">Easy Customization</a>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
-              </li>
-            </ul>
-          </div>
-          <div class="col-md-8 col-md-pull-4 probootstrap-animate" style="position: relative;">
-            <div class="probootstrap-home-showcase-wrap">
-              <div class="probootstrap-home-showcase-inner">
-                <div class="probootstrap-chrome">
-                  <div><span></span><span></span><span></span></div>
-                </div>
-                <div class="probootstrap-image-showcase">
-                  <ul class="probootstrap-images-list">
-                    <li class="active"><img src="img/img_showcase_2.jpg" alt="Image" class="img-responsive"></li>
-                    <li><img src="img/img_showcase_1.jpg" alt="Image" class="img-responsive"></li>
-                    <li><img src="img/img_showcase_2.jpg" alt="Image" class="img-responsive"></li>
-                    <li><img src="img/img_showcase_1.jpg" alt="Image" class="img-responsive"></li>
-                    <li><img src="img/img_showcase_2.jpg" alt="Image" class="img-responsive"></li>
-                    <li><img src="img/img_showcase_1.jpg" alt="Image" class="img-responsive"></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          
-        </div>
-      </div>
-    </section>
-
-    
-    <section class="probootstrap-section proboostrap-clients probootstrap-bg-white probootstrap-border-top">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6 section-heading probootstrap-animate">
-            <h2>Our Clients</h2>
-            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
-          </div>
-        </div>
-        <!-- END row -->
-        <div class="row">
-          <div class="col-md-3 col-sm-6 col-xs-6 text-center client-logo probootstrap-animate" data-animate-effect="fadeIn">
-            <img src="img/client_1.png" class="img-responsive" alt="Free Bootstrap Template by uicookies.com">
-          </div>
-          <div class="col-md-3 col-sm-6 col-xs-6 text-center client-logo probootstrap-animate" data-animate-effect="fadeIn">
-            <img src="img/client_2.png" class="img-responsive" alt="Free Bootstrap Template by uicookies.com">
-          </div>
-          <div class="clearfix visible-sm-block visible-xs-block"></div>
-          <div class="col-md-3 col-sm-6 col-xs-6 text-center client-logo probootstrap-animate" data-animate-effect="fadeIn">
-            <img src="img/client_3.png" class="img-responsive" alt="Free Bootstrap Template by uicookies.com">
-          </div>
-          <div class="col-md-3 col-sm-6 col-xs-6 text-center client-logo probootstrap-animate" data-animate-effect="fadeIn">
-            <img src="img/client_4.png" class="img-responsive" alt="Free Bootstrap Template by uicookies.com">
-          </div>
-          
-        </div>
-      </div>
-    </section>
-    <section class="probootstrap-cta">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <h2 class="probootstrap-animate" data-animate-effect="fadeInRight">We'd like to help and talk with you</h2>
-            <a href="#" role="button" class="btn btn-primary btn-lg btn-ghost probootstrap-animate" data-animate-effect="fadeInLeft">Contact Us</a>
-          </div>
-        </div>
-      </div>
-    </section>
-    <footer class="probootstrap-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12 copyright probootstrap-animate">
-            <p><small>&copy; 2017 <a href="#">uiCookies:Inspire</a>. All Rights Reserved. <br> Designed &amp; Developed with <i class="icon icon-heart"></i> by <a href="https://uicookies.com/">uicookies.com</a></small></p>
-          </div>
-        </div>
-      </div>
-    </footer>
-  
-    <!-- Modal login -->
-    <div class="modal fadeInUp probootstrap-animated" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
-      <div class="vertical-alignment-helper">
-        <div class="modal-dialog modal-md vertical-align-center">
-          <div class="modal-content">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="icon-cross"></i></button>
-            <div class="probootstrap-modal-flex">
-              <div class="probootstrap-modal-figure" style="background-image: url(img/modal_bg.jpg);"></div>
-              <div class="probootstrap-modal-content">
-                <form action="#" class="probootstrap-form">
-                  <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Email">
-                  </div> 
-                  <div class="form-group">
-                    <input type="password" class="form-control" placeholder="Password">
-                  </div> 
-                  <div class="form-group clearfix mb40">
-                    <label for="remember" class="probootstrap-remember"><input type="checkbox" id="remember"> Remember Me</label>
-                    <a href="#" class="probootstrap-forgot">Forgot Password?</a>
-                  </div>
-                  <div class="form-group text-left">
-                    <div class="row">
-                      <div class="col-md-6">
-                        <input type="submit" class="btn btn-primary btn-block" value="Sign In">
-                      </div>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 text-center probootstrap-hero-text pb0 probootstrap-animate" data-animate-effect="fadeIn">
+        <h1>Share your books!</h1>
+        <p>
+          <a href="#" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#loginModal">Log In</a>
+          <a href="#" class="btn btn-primary btn-ghost btn-lg" data-toggle="modal"  data-target="#signupModal">Sign Up</a>
+        </p>
       </div>
     </div>
-    <!-- END modal login -->
-    
-    <!-- Modal signup -->
-    <div class="modal fadeInUp probootstrap-animated" id="signupModal" tabindex="-1" role="dialog" aria-labelledby="signupModalLabel" aria-hidden="true">
-      <div class="vertical-alignment-helper">
-        <div class="modal-dialog modal-md vertical-align-center">
-          <div class="modal-content">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="icon-cross"></i></button>
-            <div class="probootstrap-modal-flex">
-              <div class="probootstrap-modal-figure" style="background-image: url(img/modal_bg.jpg);"></div>
-              <div class="probootstrap-modal-content">
-                <form action="#" class="probootstrap-form">
-                  <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Email">
-                  </div> 
-                  <div class="form-group">
-                    <input type="password" class="form-control" placeholder="Password">
-                  </div> 
-                  <div class="form-group">
-                    <input type="password" class="form-control" placeholder="Re-type Password">
-                  </div> 
-                  <div class="form-group clearfix mb40">
-                    <label for="remember" class="probootstrap-remember"><input type="checkbox" id="remember"> Remember Me</label>               
-                  </div>
-                  <div class="form-group text-left">
-                    <div class="row">
-                      <div class="col-md-6">
-                        <input type="submit" class="btn btn-primary btn-block" value="Sign Up">
-                      </div>
-                    </div>
-                   </div>                   
-                </form>
-              </div>
+          
+    <!-- ##### Latest Books Area Start ##### -->
+
+    <div class="row">
+      <div class="col-md-12 section-heading probootstrap-animate text-center">
+        <h2>Latest Books</h2>
+        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
+        <div class="row">
+          <div class="col-lg-2 col-sm-4">
+            <div class="thumbnail">
+              <img class="img-thumbnail" src="img\img_01_book.jpg">
+              <p class="text-sm-left">Title</p>
             </div>
           </div>
-        </div>
+          <div class="col-lg-2 col-sm-4">
+            <div class="thumbnail">
+              <img class="img-thumbnail" src="img\img_02_book.jpg">
+              <p class="text-sm-left">Title</p>
+            </div>
+          </div>
+          <div class="col-lg-2 col-sm-4">
+            <div class="thumbnail">
+              <img class="img-thumbnail" src="img\img_03_book.jpg">
+              <p class="text-sm-left">Title</p>
+            </div>
+          </div>
+          <div class="col-lg-2 col-sm-4">
+            <div class="thumbnail">
+              <img class="img-thumbnail" src="img\img_04_book.jpg">
+              <p class="text-sm-left">Title</p>
+            </div>
+          </div>
+          <div class="col-lg-2 col-sm-4">
+            <div class="thumbnail">
+              <img class="img-thumbnail" src="img\img_05_book.jpg">
+              <p class="text-sm-left">Title</p>
+            </div>
+          </div>
+          <div class="col-lg-2 col-sm-4">
+            <div class="thumbnail">
+              <img class="img-thumbnail" src="img\img_06_book.jpg">
+              <p class="text-sm-left">Title</p>
+            </div>
+          </div>
+      </div>
+
+       <!-- ##### Latest Books Area End ##### -->
+
+  </div>        
+</section>
+
+<section class="probootstrap-cta">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h2 class="probootstrap-animate" data-animate-effect="fadeInRight">We'd like to help and talk with you</h2>
+        <a href="contact.php" role="button" class="btn btn-primary btn-lg btn-ghost probootstrap-animate" data-animate-effect="fadeInLeft">Contact Us</a>
       </div>
     </div>
-    <!-- END modal signup -->
+  </div>
+</section>
 
-    <script src="js/scripts.min.js"></script>
-    <script src="js/custom.min.js"></script>
-  </body>
-</html>
+<?php 
+  include "includes/signupForm.php";
+  include "includes/loginForm.php";
+  include "includes/footer.php";
+?>
