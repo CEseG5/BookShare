8710c7179474b3b231ad5f196c70df8a5a56e591
ukaltrine <uka2013@outlook.com.noreply.github.com>
2018-11-10 20:50:06 +0100

    Created the page where the user can view and edit their account, view and register their books, view their rentals

--- profile.php
+++ profile.php
@@ -1,45 +1,171 @@
 <?php 
-    include "includes/head.php";
-    include "includes/navbar.php";
- ?>
- 
+include "includes/head.php";
+include "includes/navbar.php";
+?>
+
 <!DOCTYPE html>
 <html>
 <body>
-    <section class="probootstrap-hero probootstrap-xs-hero probootstrap-hero-colored">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 text-left probootstrap-hero-text">
-            <h1 class="probootstrap-animate" data-animate-effect="fadeIn"></h1>
-            <!--   -->
+  <section class="probootstrap-hero probootstrap-xs-hero probootstrap-hero-colored">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 text-left probootstrap-hero-text">
+          <h1 class="probootstrap-animate" data-animate-effect="fadeIn">My Profile</h1>
+          <!--  -->
+        </div>
+      </div>
+    </div>
+  </section>
 
+  <section class="probootstrap-section probootstrap-bg-color">
+    <div class="container">
+      <div class="col-md-12 text-left probootstrap-hero-text">
+
+        <!-- Nav tabs -->            
+        <div> 
+          <ul class="nav nav-fill nav-tabs" id="myTab" role="tablist" ata-animate-effect="fadeIn" >
+            <li class="active nav-item">
+              <a class="nav-link" id="account-tab" data-toggle="tab" href="#account" role="tab" aria-controls="account" aria-selected="false" >My Account</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="mybooks-tab" data-toggle="tab" href="#mybooks" role="tab" aria-controls="mybooks" aria-selected="false">My Books</a>
+            </li>
+            <li class="nav-item ">
+              <a class="nav-link" id="rentals-tab" data-toggle="tab" href="#rentals" role="tab" aria-controls="rentals" aria-selected="false">Current Rentals</a>
+            </li>
+          </ul>
+        </div>
+
+        <!-- Tab content -->
+        <!-- Profile tab -->
+        <div class="tab-content">
+          <div class="tab-pane active" id="account" role="tabpanel" aria-labelledby="account-tab">
+           <div class="divTable col-md-12">
+            <div class="divTableBody">
+              <div class="divTableRow ">
+                <div class="divTableCellLabel col-md-4">First Name</div>
+                <div class="divTableCell col-md-6">Emir</div>
+                <div class="divTableCell col-md-2 text-right"><a href="">Edit</a></div>
+              </div>
+              <div class="divTableRow">
+                <div class="divTableCellLabel  col-md-4">Last Name</div>
+                <div class="divTableCell col-md-6">Uka</div>
+              </div>
+              <div class="divTableRow">
+                <div class="divTableCellLabel col-md-4">Email</div>
+                <div class="divTableCell col-md-6">emir@email.com</div>
+              </div>
+            </div>
+          </div>
+          <div class="divTable col-md-12">
+            <div class="divTableBody">
+              <div class="divTableRow">
+                <div class="divTableCellLabel col-md-4">Address</div>
+                <div class="divTableCell col-md-6">Street Name, nr. 8</div>
+                <div class="divTableCell col-md-2 text-right"><a href="">Edit</a></div>
+              </div>
+              <div class="divTableRow">
+                <div class="divTableCellLabel col-md-4">City</div>
+                <div class="divTableCell col-md-6">Pristina</div>
+              </div>
+            </div>
+          </div>
+          <div class="divTable col-md-12">
+            <div class="divTableBody">
+              <div class="divTableRow">
+                <div class="divTableCell  col-md-offset-8 col-md-4  text-right"><a href="">Change Password</a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- My books tab -->
+        <div class="tab-pane" id="mybooks" role="tabpanel" aria-labelledby="mybooks-tab">
+          <div class="divTable col-md-12">
+            <div class="divTableHeading">
+              <div class="divTableRow">
+                <div class="divTableHead col-md-4" >Title</div>
+                <div class="divTableHead col-md-4">Author</div>
+                <div class="divTableHead col-md-2">Status</div>                
+                <div class="divTableHead col-md-2">&nbsp</div>
+              </div>
+            </div>
+            <div class="divTableBody">
+              <div class="divTableRow">
+                <div class="divTableCell col-md-4">cell1_1</div>
+                <div class="divTableCell col-md-4">cell2_1</div>
+                <div class="divTableCell col-md-2">cell3_1</div>
+                <div class="divTableCell col-md-2 text-right"><a href="">Edit</a></div>
+              </div>
+              <div class="divTableRow">
+                <div class="divTableCell col-md-4">cell1_2</div>
+                <div class="divTableCell col-md-4">cell2_2</div>
+                <div class="divTableCell col-md-2">cell3_2</div>
+                <div class="divTableCell col-md-2 text-right"><a href="">Edit</a></div>
+              </div>
+              <div class="divTableRow">
+                <div class="divTableCell col-md-4">cell1_3</div>
+                <div class="divTableCell col-md-4">cell2_3</div>
+                <div class="divTableCell col-md-2">cell3_3</div>
+                <div class="divTableCell col-md-2 text-right"><a href="">Edit</a></div>
+              </div>
+            </div>
+            <div class="divTableFoot">
+              <div class="divTableRow">
+                <div class="divTableCell col-md-offset-10 col-md-2 text-right"><a href="">Add Book</a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Rentals tab -->
+        <div class="tab-pane" id="rentals" role="tabpanel" aria-labelledby="rentals-tab">
+          <div class="divTable col-md-12">
+            <div class="divTableHeading">
+              <div class="divTableRow">
+                <div class="divTableHead col-md-4">Title</div>
+                <div class="divTableHead col-md-3">Author</div>
+                <div class="divTableHead col-md-2">Due Date</div>
+                <div class="divTableHead col-md-2">Status</div>                
+                <div class="divTableHead col-md-1">&nbsp</div>
+              </div>
+            </div>
+            <div class="divTableBody">
+              <div class="divTableRow">
+                <div class="divTableCell col-md-4">cell1_1</div>
+                <div class="divTableCell col-md-3">cell2_1</div>
+                <div class="divTableCell col-md-2">cell3_1</div>
+                <div class="divTableCell col-md-2">cell3_1</div>
+                <div class="divTableCell col-md-1 text-right"><a href="">Edit</a></div>
+              </div>
+              <div class="divTableRow">
+                <div class="divTableCell col-md-4">cell1_2</div>
+                <div class="divTableCell col-md-3">cell2_2</div>
+                <div class="divTableCell col-md-2">cell3_2</div>
+                <div class="divTableCell col-md-2">cell3_2</div>
+                <div class="divTableCell col-md-1 text-right"><a href="">Edit</a></div>
+              </div>
+              <div class="divTableRow">
+                <div class="divTableCell col-md-4">cell1_3</div>
+                <div class="divTableCell col-md-3">cell2_3</div>
+                <div class="divTableCell col-md-2">cell3_3</div>
+                <div class="divTableCell col-md-2">cell3_3</div>
+                <div class="divTableCell col-md-1 text-right"><a href="">Edit</a></div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </section>
 
-    <section class="probootstrap-section probootstrap-bg-white">
-      <div class="container">
+    </div>
+  </div>
+</section>
 
-
-
-
-        <!--  -->
-
-
-
-
-
-      </div>
-    </section>
- 
-    <script src="js/scripts.min.js"></script>
-    <script src="js/custom.min.js"></script>
-  </body>
+<script src="js/scripts.min.js"></script>
+<script src="js/custom.min.js"></script>
+</body>
 </html>
 
 <?php 
-  include "includes/signupForm.php";
-  include "includes/loginForm.php";
-  include "includes/footer.php";
+include "includes/signupForm.php";
+include "includes/loginForm.php";
+include "includes/footer.php";
 ?>
