31b36efe848b667985a2e19b276f0eb21a3294fc
ukaltrine <uka2013@outlook.com.noreply.github.com>
2018-11-10 20:58:20 +0100

    footer updated

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,11 @@
 <!-- Licence -->
 <footer class="probootstrap-footer">
   <div class="container">
-    <div class="row">
-      <div class="col-md-12 copyright probootstrap-animate">
-        <p><small>&copy; 2017 <a href="#">uiCookies:Inspire</a>. All Rights Reserved. <br> Designed &amp; Developed with <i class="icon icon-heart"></i> by <a href="https://uicookies.com/">uicookies.com</a></small></p>
-      </div>
+    <div class="footer-copyright text-center py-3">
+      <p><small>&copy; 2017 <a href="#">uiCookies:Inspire</a>. All Rights Reserved. <br> Designed &amp; Developed with <i class="icon icon-heart"></i> by <a href="https://uicookies.com/">uicookies.com</a></small></p>
     </div>
-  </div>
 </footer>
 
 <script src="js/scripts.min.js"></script>
 <script src="js/custom.min.js"></script>
+
