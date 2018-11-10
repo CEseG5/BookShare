903bc3adbd7ec968fbd337729850dbdcbdece363
lindorh <44001450+lindorh@users.noreply.github.com>
2018-11-10 21:55:10 +0100

    Update addBooks.php

--- addBooks.php
+++ addBooks.php
@@ -2,10 +2,13 @@
     include "includes/head.php";
     include "includes/navbar.php";
  ?>
- 
 <!DOCTYPE html>
-<html>
-<body>
+ <html>
+ <head>
+   <title>Add Books</title>
+   <link rel="stylesheet" type="text/css" href="css/style.addbook.css">
+ </head>
+ <body>
     <section class="probootstrap-hero probootstrap-xs-hero probootstrap-hero-colored">
       <div class="container">
         <div class="row">
@@ -16,26 +19,39 @@
         </div>
       </div>
     </section>
-
     <section class="probootstrap-section probootstrap-bg-white">
-      <div class="container">
-
-
-
-
-        <!-- form here  -->
-
-
-
-
-
+      <div class="container probootstrap-animate">
+              <!-- form here  -->
+        <div class="register-form">
+          <div class="main-div">
+              <div class="panel centerAlign">
+               <h2>Register Form</h2>
+               <p>Please enter required info, to register books</p>
+             </div>
+              <form id="Register" method="POST">
+                  <div class="form-group">
+                      <label for='isbn'>Enter Book ISBN</label>
+                      <input type="text" name="isbn" class="form-control" id="inputISBN" placeholder="ISBN">
+                  </div>
+                  <div class="form-group">
+                      <label for="author">Enter Author Name</label>
+                      <input type="text" name="author" class="form-control" id="inputAuthor" placeholder="Author">
+                  </div>
+                  <div class="form-group">
+                      <label for="title">Enter Book Title</label>
+                      <input type="text" class="form-control" id="inputTitle" placeholder="Title">
+                  </div>        
+                  <div class="centerAlign">
+                  <button type="submit" class="btn btn-primary">Register</button></div>
+              </form>
+          </div>
+        </div>
       </div>
     </section>
- 
     <script src="js/scripts.min.js"></script>
     <script src="js/custom.min.js"></script>
-  </body>
-</html>
+ </body>
+ </html> 
 
 <?php 
   include "includes/signupForm.php";
