9f7ffd5838e1f5fc1b5d4efb6a49265d809bb3d8
ukaltrine <uka2013@outlook.com.noreply.github.com>
2018-11-12 03:43:58 +0100

    added include statement for server connection

--- faq.php
+++ faq.php
@@ -1,70 +1,73 @@
 <?php 
-    $page_title = "Faq";
-    include "includes/head.php";
-    include "includes/navbar.php";
- ?>
- 
+include 'includes/server.php';
+?>
+
 <!DOCTYPE html>
 <html>
-<body>
-
-    <section class="probootstrap-hero probootstrap-xs-hero probootstrap-hero-colored">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 text-left probootstrap-hero-text">
-            <h1 class="probootstrap-animate" data-animate-effect="fadeIn">Frequently Ask Questions</h1>
-            <p class="probootstrap-animate" data-animate-effect="fadeIn">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <section class="probootstrap-section probootstrap-bg-white probootstrap-zindex-above-showcase">
-      <div class="container">
-        <!-- END row -->
-        <div class="row">
-          <div class="col-md-6 probootstrap-animate" data-animate-effect="fadeIn">
-            <h3>Lorem ipsum dolor sit amet?</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt repellendus minima reprehenderit tempore nihil necessitatibus impedit dolorum non maiores totam amet. Eveniet voluptatum odio dolorem corrupti nesciunt sed cum veniam.</p>
-            <h3>Incidunt earum quibusdam dolores?</h3>
-            <p>Mollitia repellendus quaerat consequuntur? Vero vel sint quia reprehenderit ex tempore soluta corrupti voluptate quae cupiditate iusto perspiciatis eveniet culpa nesciunt modi rem. Officiis tempora sequi aut enim laudantium quos.</p>
-            <h3>Hic a atque quibusdam?</h3>
-            <p>Maxime quaerat sapiente provident iste corporis aut eum dolorum sunt a ad tenetur sit non eligendi labore. Illo animi accusantium nihil deserunt qui fugit officia quisquam sint ea amet. Sit?</p>
-            <h3>Cumque fugit repellat magni?</h3>
-            <p>Laudantium illo beatae nesciunt odio eligendi vitae quis molestias illum quas facilis. Voluptatum sunt totam tempore a sit repellat aperiam culpa ratione cum explicabo beatae facere aspernatur maiores est consectetur.</p>
-            
-          </div>
-          <div class="col-md-6 probootstrap-animate" data-animate-effect="fadeIn">
-            <h3>Quibusdam voluptate?</h3>
-            <p>Alias optio? Eum beatae ea reiciendis laboriosam animi consequuntur quidem quia libero aliquam deserunt ipsam officiis saepe ad dicta accusamus et earum commodi doloribus ipsum cupiditate dolorum ullam nulla necessitatibus.</p>
-            <h3>Eveniet qui accusantium aspernatur?</h3>
-            <p>Possimus exercitationem odio repellendus quae repudiandae dolores delectus doloribus debitis maxime ab atque cum expedita minima suscipit quis perspiciatis impedit sit ratione qui beatae corrupti neque nostrum asperiores. Suscipit ullam.</p>
-            <h3>Maxime consequatur ipsam?</h3>
-            <p>Quam expedita sed velit id saepe aliquid natus sunt eum modi et dolorum ex incidunt debitis itaque voluptatem voluptatum perspiciatis molestiae cupiditate nemo reiciendis laborum tenetur placeat cum! Ducimus recusandae.</p>
-          </div>
-        </div>
-        
-      </div>
-    </section>
-    
-    <section class="probootstrap-cta">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <h2 class="probootstrap-animate" data-animate-effect="fadeInRight">We'd like to help and talk with you</h2>
-            <a href="#" role="button" class="btn btn-primary btn-lg btn-ghost probootstrap-animate" data-animate-effect="fadeInLeft">Contact Us</a>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <script src="js/scripts.min.js"></script>
-    <script src="js/custom.min.js"></script>
-  </body>
-</html>
-
 <?php 
+$page_title = "Faq";
+include "includes/head.php";
+?>
+
+<body>
+  <?php include "includes/navbar.php"; ?>
+  <section class="probootstrap-hero probootstrap-xs-hero probootstrap-hero-colored">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 text-left probootstrap-hero-text">
+          <h1 class="probootstrap-animate" data-animate-effect="fadeIn">Frequently Ask Questions</h1>
+          <p class="probootstrap-animate" data-animate-effect="fadeIn">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="probootstrap-section probootstrap-bg-white probootstrap-zindex-above-showcase">
+    <div class="container">
+      <!-- END row -->
+      <div class="row">
+        <div class="col-md-6 probootstrap-animate" data-animate-effect="fadeIn">
+          <h3>Lorem ipsum dolor sit amet?</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt repellendus minima reprehenderit tempore nihil necessitatibus impedit dolorum non maiores totam amet. Eveniet voluptatum odio dolorem corrupti nesciunt sed cum veniam.</p>
+          <h3>Incidunt earum quibusdam dolores?</h3>
+          <p>Mollitia repellendus quaerat consequuntur? Vero vel sint quia reprehenderit ex tempore soluta corrupti voluptate quae cupiditate iusto perspiciatis eveniet culpa nesciunt modi rem. Officiis tempora sequi aut enim laudantium quos.</p>
+          <h3>Hic a atque quibusdam?</h3>
+          <p>Maxime quaerat sapiente provident iste corporis aut eum dolorum sunt a ad tenetur sit non eligendi labore. Illo animi accusantium nihil deserunt qui fugit officia quisquam sint ea amet. Sit?</p>
+          <h3>Cumque fugit repellat magni?</h3>
+          <p>Laudantium illo beatae nesciunt odio eligendi vitae quis molestias illum quas facilis. Voluptatum sunt totam tempore a sit repellat aperiam culpa ratione cum explicabo beatae facere aspernatur maiores est consectetur.</p>
+
+        </div>
+        <div class="col-md-6 probootstrap-animate" data-animate-effect="fadeIn">
+          <h3>Quibusdam voluptate?</h3>
+          <p>Alias optio? Eum beatae ea reiciendis laboriosam animi consequuntur quidem quia libero aliquam deserunt ipsam officiis saepe ad dicta accusamus et earum commodi doloribus ipsum cupiditate dolorum ullam nulla necessitatibus.</p>
+          <h3>Eveniet qui accusantium aspernatur?</h3>
+          <p>Possimus exercitationem odio repellendus quae repudiandae dolores delectus doloribus debitis maxime ab atque cum expedita minima suscipit quis perspiciatis impedit sit ratione qui beatae corrupti neque nostrum asperiores. Suscipit ullam.</p>
+          <h3>Maxime consequatur ipsam?</h3>
+          <p>Quam expedita sed velit id saepe aliquid natus sunt eum modi et dolorum ex incidunt debitis itaque voluptatem voluptatum perspiciatis molestiae cupiditate nemo reiciendis laborum tenetur placeat cum! Ducimus recusandae.</p>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+  <section class="probootstrap-cta">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="probootstrap-animate" data-animate-effect="fadeInRight">We'd like to help and talk with you</h2>
+          <a href="#" role="button" class="btn btn-primary btn-lg btn-ghost probootstrap-animate" data-animate-effect="fadeInLeft">Contact Us</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <script src="js/scripts.min.js"></script>
+  <script src="js/custom.min.js"></script>
+  
+  <?php 
   include "includes/signupForm.php";
   include "includes/loginForm.php";
   include "includes/footer.php";
-?>
+  ?>
+</body>
+</html>
