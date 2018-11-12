f9cfd2e32e4bb1f47ee18d2fb4c49d1267ea9872
ukaltrine <uka2013@outlook.com.noreply.github.com>
2018-11-12 03:15:34 +0100

    php script to change navbar after login

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,6 +1,6 @@
 <!-- Fixed navbar -->
 <!-- navbar-fixed-top  -->
-    
+
 <nav class="navbar probootstrap-megamenu navbar-default probootstrap-navbar">
   <div class="container">
     <div class="navbar-header">
@@ -17,9 +17,19 @@
       <ul class="nav navbar-nav navbar-right">
       	<li><a href="faq.php">FAQ</a></li>
         <li><a href="contact.php">Contact</a></li>
-         <li class="probootstra-cta-button"><a href="#" class="btn" data-toggle="modal" data-target="#loginModal">Log in</a></li>
-        <li class="probootstra-cta-button last"><a href="#" class="btn btn-ghost" data-toggle="modal" data-target="#signupModal">Sign up</a></li>
-      </ul>
+
+        <?php  if (isset($_SESSION['email'])) : ?>
+          <li><a href="profile.php">Profile</a></li>
+          <li class="probootstra-cta-button last"><a href="index.php?logout='1'"  class="btn btn-ghost">Log Out</a></li>
+          <?php else: ?>
+            <li class="probootstra-cta-button"><a href="#" class="btn" data-toggle="modal" data-target="#loginModal">Log in</a></li>
+            <li class="probootstra-cta-button last"><a href="#" class="btn btn-ghost" data-toggle="modal" data-target="#signupModal">Sign up</a></li>
+          <?php endif; ?>
+
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
+
+  <?php include 'includes/errors.php'; ?>
+  <?php include 'includes/logout.php'; ?>
