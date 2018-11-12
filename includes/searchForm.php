918319369520212096080a92d60cd481cc715eff
ukaltrine <uka2013@outlook.com.noreply.github.com>
2018-11-12 03:02:50 +0100

    search bar/form

---
+++ includes/searchForm.php
@@ -0,0 +1,6 @@
+<form class="searchBar col-md-12">
+	<div class="row">
+		<input type="text" class="inputSearch col-md-8 " placeholder="Search book by title, author, ISBN...">
+		<button type="submit" name="search" class="btn btn-primary col-md-4">Search</button>
+	</div>
+</form>
