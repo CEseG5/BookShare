7b571554a4eb9fd9f8a78476f68e74d55a16f21d
lindorh <44001450+lindorh@users.noreply.github.com>
2018-11-16 02:01:25 +0100

    Update profile.php

--- profile.php
+++ profile.php
@@ -32,10 +32,6 @@ include "includes/head.php";
           <ul class="nav nav-fill nav-tabs" id="myTab" role="tablist" ata-animate-effect="fadeIn" >
             <li class="active nav-item">
               <a class="nav-link" id="account-tab" data-toggle="tab" href="#account" role="tab" aria-controls="account" aria-selected="false" >My Account</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" id="mybooks-tab" data-toggle="tab" href="#mybooks" role="tab" aria-controls="mybooks" aria-selected="false">My Books</a>
-            </li>
             <li class="nav-item ">
               <a class="nav-link" id="rentals-tab" data-toggle="tab" href="#rentals" role="tab" aria-controls="rentals" aria-selected="false">Current Rentals</a>
             </li>
@@ -102,47 +98,7 @@ include "includes/head.php";
           </div>
         </div>
 		<!-- My books tab -->
-        <div class="tab-pane" id="mybooks" role="tabpanel" aria-labelledby="mybooks-tab">
-          <div class="divTable col-md-12">
-            <div class="divTableHeading">
-              <div class="divTableRow">
-                <div class="divTableHead col-md-2"></div>
-                <!-- <div class="divTableHead col-md-2">ISBN</div> -->
-                <div class="divTableHead col-md-3">Author</div>
-                <div class="divTableHead col-md-3">Title</div>                
-                <div class="divTableHead col-md-2">Status</div>                
-                <div class="divTableHead col-md-2">&nbsp</div>
-              </div>
-            </div>
-            <div class="divTableBody">
-
-              <?php 
-                $query = "select * from register_books";
-                $result = mysqli_query($connection, $query);
-                
-                  while ($row = mysqli_fetch_assoc($result)){
-                    echo "<div class='divTableRow'>";
-                    echo "<div class='divTableCell col-md-2'><img src='img/".$row['img_path']."'></div>";
-                    // echo "<div class='divTableCell col-md-1'>". $row['isbn'] ."</div>";   
-                    echo "<div class='divTableCell col-md-3'>". $row['author'] ."</div>";   
-                    echo "<div class='divTableCell col-md-3'>". $row['title'] ."</div>";   
-                    echo "<div class='divTableCell col-md-2'><select  name=''>
-                                                              <option>Registered</option>
-                                                              <option>Available</option>
-                                                              <option>Not available</option>
-                                                            </select></div>";   
-                    echo "<div class='divTableCell col-md-2 text-right'><a href='changeBookStatus.php?isbn={$row['isbn']}'>Save</a></div>";
-                    echo "</div>";
-                }
-               ?>
-            </div>
-            <div class="divTableFoot">
-              <div class="divTableRow">
-                <div class="divTableCell col-md-offset-10 col-md-2 text-right"><a href="addBooks.php">Add Book</a></div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <!--Books in new file-->
         <!-- Rentals tab -->
         <div class="tab-pane" id="rentals" role="tabpanel" aria-labelledby="rentals-tab">
           <div class="divTable col-md-12">
