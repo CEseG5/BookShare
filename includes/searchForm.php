cbb3f46cd6a1d9ba0d08fa4756b280c992f03e65
BesarZh <besar.zhubi@student.cacttus.education>
2018-12-05 17:01:18 +0100

    searchForm updated

--- includes/searchForm.php
+++ includes/searchForm.php
@@ -5,13 +5,14 @@
 			<?php
 			$query = "select * from cities";
 			$result = mysqli_query($connection, $query);
-
+			$search = $_POST['bsearch'];
 			while ($row = mysqli_fetch_assoc($result)){
+
 				echo "<option value='{$row['id']}'".($row['id'] == $_POST['city'] ? 'selected': ''). ">".$row['name']."</option>";
 			}
 			?>
 		</select>
-		<input type="text" class="inputSearch col-md-8" id="searchbox" placeholder="Search book by title, author, ISBN..." name="bsearch">
+		<input type="text" class="inputSearch col-md-8" id="searchbox" value="<?php echo $search; ?>" placeholder="Search book by title, author, ISBN..." name="bsearch">
 		<button type="submit" name="search" class="btn btn-primary col-md-2">Search</button>
 	</div>
 </form>
