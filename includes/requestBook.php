48ca7183e61c00c9ab97e934e680017182de052c
lindorh <44001450+lindorh@users.noreply.github.com>
2018-12-17 21:07:24 +0100

    added logic behind requests, validation not finished yet

--- includes/requestBook.php
+++ includes/requestBook.php
@@ -1,2 +1,67 @@
  <?php 
-    if (isset($_POST['requestBook'])) {}
+      
+      session_start();
+      $session_id = $_SESSION['id'];
+
+      $connection = mysqli_connect('localhost', 'root', '', 'bookshare');
+
+      $return_date = $_POST['return_date'];
+      $borrower_id = $_POST['borrower_of_book'];
+      $owner_id = $_POST['owner_of_book'];
+      $book_id = $_POST['book_id_r'];
+
+      $query_select = "SELECT book_id, borrower_id FROM requests where borrower_id = '$session_id' AND book_id = '$book_id';";
+
+      $result = mysqli_query($connection, $query_select);
+
+      $row = mysqli_num_rows($result);
+
+      if(empty($return_date)){
+         ?>
+         <script type="text/javascript">
+            alert('Data field is empty!');
+         </script>
+         <?php
+         exit();
+      }
+     elseif(!strtotime($return_date)){
+         ?>
+         <script type="text/javascript">
+            alert('Data is not valid!');
+         </script>
+         <?php
+         exit();
+     }
+      elseif($row >= 1){
+         ?>
+         <script type="text/javascript">
+            alert('Request already sent!');
+         </script>
+         <?php
+         exit();
+      }
+      elseif(strtotime($return_date) < strtotime('now') ) {
+         ?>
+         <script type="text/javascript">
+            alert('Date is in invalid format or is in the past!');
+         </script>
+         <?php
+         exit();
+      }
+
+      else {
+          $return_date = date("Y-m-d", strtotime($return_date));
+
+            $sql = "INSERT INTO requests (borrower_id, owner_id, book_id, return_date) VALUES ('$borrower_id','$owner_id','$book_id','$return_date');";
+
+            if(mysqli_query($connection, $sql)){
+               ?>
+               <script type="text/javascript">
+                  alert('Request successfully sent!');
+               </script>
+               <?php
+               exit();
+            } 
+      }
+
+ ?>
