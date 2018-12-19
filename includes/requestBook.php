c45ecde8ad5e3de2b403518866bdf8418b773042
lindorh <44001450+lindorh@users.noreply.github.com>
2018-12-19 16:12:50 +0100

    added regex check date if it's correctly inserted(must be inserted as placeholder suggest otherwise it throws error!)

--- includes/requestBook.php
+++ includes/requestBook.php
@@ -16,6 +16,8 @@
 
       $row = mysqli_num_rows($result);
 
+
+
       if(empty($return_date)){
          ?>
          <script type="text/javascript">
@@ -27,7 +29,14 @@
      elseif(!strtotime($return_date)){
          ?>
          <script type="text/javascript">
-            alert('Data is not valid!');
+            alert('Date is not valid!');
+         </script>
+         <?php
+         exit();
+     } elseif(!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$return_date)){
+         ?>
+         <script type="text/javascript">
+            alert('Incorrect Date input!');
          </script>
          <?php
          exit();
@@ -39,7 +48,7 @@
          </script>
          <?php
          exit();
-      }
+      } 
       elseif(strtotime($return_date) < strtotime('now') ) {
          ?>
          <script type="text/javascript">
@@ -47,21 +56,21 @@
          </script>
          <?php
          exit();
-      }
+      } 
 
       else {
           $return_date = date("Y-m-d", strtotime($return_date));
 
-            $sql = "INSERT INTO requests (borrower_id, owner_id, book_id, return_date) VALUES ('$borrower_id','$owner_id','$book_id','$return_date');";
+                  $sql = "INSERT INTO requests (borrower_id, owner_id, book_id, return_date) VALUES ('$borrower_id','$owner_id','$book_id','$return_date');";
 
-            if(mysqli_query($connection, $sql)){
-               ?>
-               <script type="text/javascript">
-                  alert('Request successfully sent!');
-               </script>
-               <?php
-               exit();
-            } 
-      }
+                  if(mysqli_query($connection, $sql)){
+                     ?>
+                     <script type="text/javascript">
+                        alert('Request successfully sent!');
+                     </script>
+                     <?php
+                     exit();
+             }
+          }
 
  ?>
