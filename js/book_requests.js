6cf0a4dc6ef5d7e87210653cb323c151888861e9
lindorh <44001450+lindorh@users.noreply.github.com>
2018-12-24 11:27:40 +0100

    updated validation, depending on what action you take modal will decide what to do!

--- js/book_requests.js
+++ js/book_requests.js
@@ -19,14 +19,13 @@
           
           e.preventDefault();
           e.stopImmediatePropagation();
-          
+          let removable_fields = $("#removable-fields");
           let return_date = $("input[name=returnDate]").val();
           let city_address = $("input[name=city_address_r]").val();
           let owner_of_book = $("input[name=owner_id_r]").val();
           let borrower_of_book = $("input[name=borrower_id_r]").val();
           let book_id_r = $("input[name=book_id_r]").val();
           let submit = $("#btn_save").val();
-
 
           $.ajax({url:"includes/requestBook.php",
                   method:"POST",
@@ -37,9 +36,24 @@
                   book_id_r:book_id_r
               },
               success:function(data){
-                $('#message').html(data).show().delay(3000).fadeOut();
-                $("#rentModal").modal('hide');
-                $("#frm")[0].reset();
+                let result = $.trim(data);
+                if(result == '<span class="alert alert-danger"> Empty Date</span>' || result == '<span class="alert alert-danger"> Date is not valid </span>' || result == '<span class="alert alert-danger"> Date inputed is not in required format! </span>' || result == '<span class="alert alert-danger"> Date entered is in the past! </span>') {
+                  $('#message').html(data).show().delay(3000).fadeOut();
+                  $("input[name=returnDate]").val('');
+                } else if(result == '<span class="alert alert-danger"> The Request for this book has already been sent! </span>') {
+                  $('#message').html(data).show().delay(5000).fadeOut();;
+                  $("#removable-fields").hide();
+                  $("#btn_save").val('').hide();
+                  $("#btn_close").val('').hide();
+                  setTimeout(() => {$("#rentModal").modal('hide');}, 4000);
+                  setTimeout(() => {location.reload();}, 4000);
+                } else {
+                  $('#message').html(data).show();
+                  $("#removable-fields").hide();
+                  $("#btn_save").val('').hide();
+                  $("#btn_close").val('').hide();
+                  setTimeout(() => {window.location.replace('books.php#sent_requests');}, 4000);
+                }
               }
             });
 
