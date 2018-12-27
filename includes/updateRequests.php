1a53b014dc5fa5b865803fc6ec61f0dae184f52f
ukaltrine <uka2013@outlook.com>
2018-12-27 14:31:47 +0100

    changed condition/removed variables

--- includes/updateRequests.php
+++ includes/updateRequests.php
@@ -4,32 +4,27 @@
 	$owner_id = $_SESSION['id'];
 	$borrower_id = $_POST['borrowerId'];
 	$book_id = $_POST['bookId'];
-	$prevState = $_POST['prevState'];
-	$state = $_POST['state'];
 
-	if(isset($_POST['updateRequest'])) {
+	if(isset($_POST['approve'])) {
 
-		if($state === 'rejected'){
-			$query = "UPDATE requests SET `is_answered` = '{$state}' WHERE `owner_id` = {$owner_id} AND `book_id` = {$book_id}  AND borrower_id =  {$borrower_id} ";
-		}
+		$query = "UPDATE requests
+				SET `is_answered` = CASE 
+				WHEN `borrower_id`= $borrower_id THEN 'approved' 
+				ELSE 'rejected' 
+				END
+				WHERE ( `owner_id` =  $owner_id  AND `book_id` = $book_id )";
 
-		if($state === 'approved'){
-			$query = "UPDATE requests
-						SET `is_answered` = CASE 
-    					WHEN `borrower_id`= $borrower_id THEN '{$state}' 
-    					ELSE 'rejected' 
-    					END
-						WHERE ( `owner_id` =  $owner_id  AND `book_id` = $book_id )";
-		}
-		
-		$result = mysqli_query($connection, $query);
-		if($result){
-			session_start();
-			$_SESSION['msg'] = "Yey!";
-			header('location: ../books.php#requests');
-		}
+	}else if(isset($_POST['reject'])){
+		$query = "UPDATE requests SET `is_answered` = 'rejected' WHERE `owner_id` = {$owner_id} AND `book_id` = {$book_id}  AND borrower_id =  {$borrower_id} ";
+
 	}else {
-		header('location: ../books.php#requests');
+		header('location: ../books.php');
 	}
 
+	$result = mysqli_query($connection, $query);
+		if($result){
+			session_start();
+			$_SESSION['msg'] = "Updated!";
+			header('location: ../books.php');
+	}
  ?>
